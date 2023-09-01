de431d7a97c76e0c434f0fca8e9aa6e075783758
epiiaddone <lll@yahoo.com>
2023-09-01 16:38:00 +0800

    split css into component files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import Game from './components/Game';
 import { ThemeProvider } from './context/theme_context';
 import { LessonProvider } from './context/lesson_context';
+
+import './index.css';
+import './css/answer-buttons.css';
+import './css/game-over.css';
+import './css/header.css';
+import './css/highlight.css';
+import './css/kanji-display.css';
+import './css/lesson-select.css';
+import './css/score-colors.css';
+import './css/score.css';
+import './css/theme-toggle.css';//import last as overwrites
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
